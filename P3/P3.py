2da475465c8e542cc267ca332cfedbd52f65a9b8
Alejandro Fontal <jandrofontal@gmail.com>
2016-11-07 21:30:53 +0100

    Added more things

--- P3/P3.py
+++ P3/P3.py
@@ -7,7 +7,7 @@ Script to solve P3
 """
 
 from sys import argv
-
+import numpy as np
 
 
 def parse_fastq_file(filename):
@@ -21,23 +21,27 @@ def parse_fastq_file(filename):
     file = filename.readlines()
 
     seqs_dict = {}
+    is_seq = False
+    is_qc = False
     for line in file:
         if line.startswith("@"):
-            seq = True
-        elif seq == True:
+            is_seq = True
+
+        elif is_seq:
             curr_seq = line.strip()
             seqs_dict[curr_seq] = []
-            seq = False
+            is_seq = False
+
         elif line.startswith("+"):
-            qc = True
-            continue
-        elif qc == True:
+            is_qc = True
+
+        elif is_qc:
             qc_vals = []
             for char in line.strip():
-                qual = ord(char) - 64
+                qual = ord(char) - 64  # Convert Illumina scores to 0-41 scores
                 qc_vals.append(qual)
                 seqs_dict[curr_seq] = qc_vals
-            qc = False
+            is_qc = False
 
     return seqs_dict
 
@@ -50,8 +54,30 @@ def fastq_stats(fastq_dict):
     lengths = map(len, fastq_dict.keys())
     max_length = max(lengths)
     min_length = min(lengths)
-    avg_length = mean(lengths)
+    avg_length = np.mean(lengths)
 
+    qual_dict = {}
+
+    for read in fastq_dict:
+        quality_list = fastq_dict[read]
+        for idx, value in enumerate(quality_list):
+            if idx in qual_dict.keys():
+                qual_dict[idx].append(value)
+            else:
+                qual_dict[idx] = [value]
+
+    for i in range(max_length):
+        qual_dict[i] = round(np.mean(qual_dict[i]), 2)
+
+    print "\nThe maximum sequence length is {}\n".format(max_length)
+    print "The minimum sequence length is {}\n".format(min_length)
+    print "The average sequence length is {}\n".format(avg_length)
+    print "The average read quality per position is:\n "
+    print "{}  {}\n".format("Position".center(15),
+                            "Average Quality".center(15))
+    for i in range(max_length):
+        print "{}     {}".format(str(i+1).center(15),
+                                 str(qual_dict[i]).ljust(15))
 
 
 
@@ -60,7 +86,7 @@ def fastq_stats(fastq_dict):
 
 if __name__ == "__main__":
 
-    seqs_dict = parse_fastq_file("tomatosample.fq")
-
-    lengths = map(len, seqs_dict.keys())
-    print max(lengths)
+    #filename = argv[1]
+    filename = "tomatosample.fq"
+    seqs_dict = parse_fastq_file(filename)
+    fastq_stats(seqs_dict)
